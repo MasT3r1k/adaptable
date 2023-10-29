ad933cf89e2cae9df2af7452fcd3edb308b36cad
MasT3r1k <master0123@centrum.cz>
2023-10-29 19:08:16 +0100

    ..

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 export default {
   database: {
-    url: 'mongodb+srv://schoolingo-main-db-01a2b2af0fa:qCZBYmU24cPqjpc85tdz2xSGWYXCr4@prod-us-central1-2.ih9la.mongodb.net/schoolingo-main-db-01a2b2af0fa',
+    url: 'mongodb+srv://schooling2-main-db-03e8aafb523:P9n7XnUAB2WBZgYGAfj39dmAg6XA8e@prod-us-central1-2.ih9la.mongodb.net/schooling2-main-db-03e8aafb523',
     host: 'prod-us-central1-2.ih9la.mongodb.net',
     port: 3306,
-    username: 'schoolingo-main-db-01a2b2af0fa',
-    password: 'qCZBYmU24cPqjpc85tdz2xSGWYXCr4',
-    databaseName: 'schoolingo-main-db-01a2b2af0fa',
+    username: 'schooling2-main-db-03e8aafb523',
+    password: 'P9n7XnUAB2WBZgYGAfj39dmAg6XA8e',
+    databaseName: 'schooling2-main-db-03e8aafb523',
   },
   application: {
       port: 3000
